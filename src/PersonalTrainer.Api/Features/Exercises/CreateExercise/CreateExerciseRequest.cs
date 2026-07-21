cb716350ad6c5bb23a3faa9f9229829c8ef45864
OlenaBack <olena.back@gmail.com>
2026-07-21 08:29:55 +0200

    Delete src/PersonalTrainer.Api/Features/Exercises directory

--- src/PersonalTrainer.Api/Features/Exercises/CreateExercise/CreateExerciseRequest.cs
+++
@@ -1,3 +0,0 @@
-namespace PersonalTrainer.Api.Features.Exercises.CreateExercise;
-
-public sealed record CreateExerciseRequest(string Name, IReadOnlyList<string>? Tags);
--- src/PersonalTrainer.Api/Features/Exercises/ExerciseResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace PersonalTrainer.Api.Features.Exercises;
-
-public sealed record ExerciseResponse(
-    Guid Id,
-    string Name,
-    IReadOnlyList<string> Tags,
-    DateTime CreatedAtUtc);
